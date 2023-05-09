727a635a13f222946ed568cf175a7df7e4e9d3a7
Adelmar92 <adelmar92@gmail.com>
2023-05-09 12:04:42 -0500

    Initial commit

---
+++ src/test/java/com/adelmar/mongoDBClient/MongoDbClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adelmar.mongoDBClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
